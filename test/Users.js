fd4df88341a26421df7c082550e6bf9540bf9ae9
Luke Opperman <luke@opperman.net>
2016-02-09 12:10:57 -0500

    And route existence tests

--- test/Users.js
+++ test/Users.js
@@ -37,5 +37,14 @@ describe("Users", () => {
       app.get('templater').provide.calledWith('template', 'user-profile').should.be.true
       app.get('templater').provide.calledWith('template', 'user-forgot-email').should.be.true
     });
+    it("should provide routes", () => {
+      app.get('router').provide.calledWith('setRoute', 'GET', '/logout').should.be.true
+      app.get('router').provide.calledWith('setRoute', 'GET', '/forgot').should.be.true
+      app.get('router').provide.calledWith('setRoute', 'POST', '/forgot').should.be.true
+      app.get('router').provide.calledWith('setRoute', 'GET', '/login-link').should.be.true
+      app.get('router').provide.calledWith('setRoute', 'GET', '/profile').should.be.true
+      app.get('router').provide.calledWith('setRoute', 'GET', '/login').should.be.true
+      app.get('router').provide.calledWith('setRoute', 'POST', '/profile/save').should.be.true
+    });
   });
 });
