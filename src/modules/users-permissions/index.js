1bdb607aa05406f89196bc65a3ebb3099e82cac7
Luke Opperman <luke@opperman.net>
2017-02-27 11:51:38 -0500

    Object permissions with a custom method for object role lookup

--- src/modules/users-permissions/index.js
+++ src/modules/users-permissions/index.js
@@ -43,7 +43,7 @@ class UsersPermissions extends HasModels {
    * Register a permission
    * @param {string} name Permission name
    * @param {string|array} [defaultRoles] Default role name(s) to contain this permission
-   * @param {object} [objectModel] Collection of ObjectRoleModel for object permissions
+   * @param {object} [objectModel] Collection of ObjectRoleModel for object permissions, or function (objectId, user) to return the roles a user has for a given object
    */
   register(name, defaultRoles=null, objectModel=null) {
     this._permissions[name] = {
@@ -68,6 +68,7 @@ class UsersPermissions extends HasModels {
    * @param {function|string} [handler|route] A handler or route to wrap with a permission check
    * @param {string} name Permission name
    * @param {object} [objectParam] URL param for object permissions object key
+   * @returns {function} Wrapped handler for method handlers
    */
   guard(handler, name, objectParam=null) {
     if (_.isString(handler)) {
@@ -84,6 +85,7 @@ class UsersPermissions extends HasModels {
    * @param {function|string} [handler|route] A handler or route to wrap with a permission check
    * @param {object} [objectParams] Mapping of req params to ObjectRoleModel identity
    * @param {string|array} [roleName] Default role name(s) to contain this permission
+   * @returns {function} Wrapped handler for method handlers
    */
   allow(name, handler = null, objectParams = null, roleName = null) {
     let coll, param
@@ -152,9 +154,13 @@ class UsersPermissions extends HasModels {
   async _setObjectRoles(user, permission, objectId) {
     let objectModel = this._permissions[permission].objectModel
     if (!objectModel) return
-    
-    let model = await storage.getModel(objectModel)
-    let roles = await model.find({user: user.id, object: objectId}).populate('role')
+    let roles
+    if (_.isString(objectModel)) {
+      let model = await storage.getModel(objectModel)
+      roles = await model.find({user: user.id, object: objectId}).populate('role')
+    } else {
+      roles = await objectModel(objectId, user)
+    }
     user.permissions.addRoles(_.pluck(roles, 'role'), objectId)
   }
 
